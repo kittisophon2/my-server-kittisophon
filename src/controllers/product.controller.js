d975d7766d98f3218bfde6651466c41added9321
peach <kittisophon.su872@gmail.com>
2024-12-21 18:14:08 +0700

    first commit

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,11 +1,59 @@
-exports.get = (req,res) =>{
-    res.send("Get product here");
-}
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-exports.getById = (req,res) =>{
-    res.send("Get product here " + req.params.id);
-}
+exports.get = async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+};
 
-exports.create = (req,res) =>{
-    res.send("Create a product ");
-}
+exports.getById = async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.json(product);
+};
+
+exports.create = async (req, res) => {
+  const { category_id, name, price, description, unit_in_stock } = req.body;
+  const product = await prisma.product.create({
+    data: {
+      category_id,
+      name,
+      price,
+      description,
+      unit_in_stock,
+    },
+  });
+  res.json(product);
+};
+
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const { category_id, name, price, description, unit_in_stock } = req.body;
+  const product = await prisma.product.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      category_id,
+      name,
+      price,
+      description,
+      unit_in_stock,
+    },
+  });
+  res.json(product);
+};
+
+exports.delete = async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.json(product);
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-require('dotenv').config({path: './config.env'});
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
